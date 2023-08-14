bb2ce1aa7003204b51bd482d97419ce18a8fbc57
pythonguru101 <pythonguru101@gmail.com>
2023-08-15 00:33:58 +0600

    Adding files :apple:

---
+++ Assets/themes/vc-hello-elementor-child/script3781.js
@@ -0,0 +1,9 @@
+// Feel free to place any JS you need in here! Included is a jQuery ready function that you should really use for any jQuery-related code.
+
+jQuery(document).ready(function($){
+
+	$('[href="#"]').click(function(event) {
+		event.preventDefault();
+	});
+
+});
